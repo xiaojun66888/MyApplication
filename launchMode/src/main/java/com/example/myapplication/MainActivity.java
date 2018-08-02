119b39bf6b0756dcbd1d47a79f83ef3985aebd2e
werewolf <969333864@qq.com>
2018-08-02 15:11:59 +0800

    更新下

--- launchMode/src/main/java/com/example/myapplication/MainActivity.java
+++ launchMode/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -15,23 +16,42 @@ import butterknife.OnClick;
 public class MainActivity extends Activity {
 
 
-    @InjectView(R.id.btn_skip)
-    Button btnSkip;
+    @InjectView(R.id.btn_skip1)
+    Button btnSkip1;
+    @InjectView(R.id.btn_skip2)
+    Button btnSkip2;
+    @InjectView(R.id.btn_skip3)
+    Button btnSkip3;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+        int state = SpUtils.getSPValue(this,"s",1);
+        if(state==1){
+            setTheme(R.style.Default_TextSize_Small);
+        }else if(state==2){
+            setTheme(R.style.Default_TextSize_Middle);
+        }else{
+            setTheme(R.style.Default_TextSize_Big);
         }
+
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
     }
 
-    @OnClick(R.id.btn_skip)
-    public void onViewClicked() {
+
+    @OnClick({R.id.btn_skip1, R.id.btn_skip2, R.id.btn_skip3})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_skip1:
+                SpUtils.putSPValue(this,"s",1);
+                break;
+            case R.id.btn_skip2:
+                SpUtils.putSPValue(this,"s",2);
+                break;
+            case R.id.btn_skip3:
+                SpUtils.putSPValue(this,"s",3);
+                break;
+        }
     }
 }
---
+++ launchMode/src/main/java/com/example/myapplication/VideoActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class VideoActivity extends Activity {
+
+    @InjectView(R.id.wv_device_history)
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        ButterKnife.inject(this);
+        initWebView();
+    }
+
+    private void initWebView() {
+        WebSettings settings = mWebView.getSettings();
+        settings.setDefaultTextEncodingName("UTF-8");
+        // 设置支持JS
+        settings.setJavaScriptEnabled(true);
+        // 设置缓存
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setAppCacheEnabled(true);
+        mWebView.requestFocus();
+        // 设置适应屏幕
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setSupportZoom(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setDisplayZoomControls(false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        } else {
+            settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+        }
+
+        String head ="<head><style>video{width: 100%;  height: 480;}object{width: 100%;  height: 480;}</style></head>";
+//       String news_content ="<video class=\"edui-upload-video  vjs-default-skin video-js\" controls=\"\" preload=\"none\" width=\"420\" height=\"280\" src=\"http://data.vod.itc.cn/?new=/92/78/KK9114TOJF7TTnDfy47k0A.mp4&vid=103494822&plat=17&mkey=y22k7FCa3rA0U_UcQPDy_nwgBLcldWk2&ch=null&user=api&uid=1608272337357415&SOHUSVP=Gh8YyutaCGBl_f4sdHzCtXTO_Tk9uhIOOsbcfn3xWzg&pt=1&prod=56&pg=1&eye=0&cv=1.0.0&qd=68000&src=11050001&ca=4&cateCode=133&_c=1&appid=tv\" data-setup=\"{}\"></video>";
+
+       String news_content = "<object data=\"http://vodcdn.video.taobao.com/oss/taobao-ugc/d5d0602f0a3e448aa4d41b7fd4d91216/1492340273/video.mp4\">";
+       String html = "<html>"+head+"<body>" + news_content + "</body></html>";
+        mWebView.loadDataWithBaseURL(null, html, "text/html", "UTF-8", null);
+    }
+}
